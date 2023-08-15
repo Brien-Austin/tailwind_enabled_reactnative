0e2243236eb9831a27efa9cd5a59bc27525a133d
Brien-Austin <brienaustinclayton@gmail.com>
2023-08-15 10:27:31 +0530

    React Native with Tailwind Integrated

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import tw from "twrnc";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <TouchableOpacity>
+        <Text className="text-3xl text-green-100  ">k me</Text>
+      </TouchableOpacity>
+
+      <StatusBar style="light" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
